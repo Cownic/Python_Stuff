604af4489246bf54170e09ff75ed7a4cddafea21
Kah Hoa <Lewkh1999@gmail.com>
2023-02-21 20:07:00 +0800

    madlib code

---
+++ madlib.py
@@ -0,0 +1,15 @@
+# Learning how to use f' to do string concentenation in python
+
+name = input("Please enter your name: ")
+
+print(f"Hello, Welcome to Madlib, {name}")
+
+exclaimation=input("Please enter an exclaimation below: ")
+adverb=input("Please enter a adverb: ")
+noun=input("Please enter a noun below: ")
+adjective=input("Please enter a adjective below: ")
+
+phase = f'{exclaimation} ! he said {adverb} as he jumped into his convertible {noun} and drove off with his wife'
+
+print(phase)
+  
